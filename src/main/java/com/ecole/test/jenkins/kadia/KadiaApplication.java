b0e526bed819b94a0410286aa1f86b7b369c7d80
KadiataB <kadiatab79@gmail.com>
2025-09-20 02:29:07 +0000

    initialisation du projet jenkins pipeline

---
+++ src/main/java/com/ecole/test/jenkins/kadia/KadiaApplication.java
@@ -0,0 +1,13 @@
+package com.ecole.test.jenkins.kadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecole/test/jenkins/kadia/KadiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecole.test.jenkins.kadia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
